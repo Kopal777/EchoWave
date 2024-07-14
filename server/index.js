cee200ad570dec1a11eb5ca85d687b7a6b6627e8
Kopal <yadavkopal436@gmail.com>
2024-07-14 19:20:27 +0530

    Update client files

--- server/index.js
+++ server/index.js
@@ -15,6 +15,9 @@ const io = new Server(server, {
         methods: ["GET", "POST"]
     }
 });
+app.get('/', (req, res)=>{
+    res.json("hello");
+})
 
 io.on("connection", (socket)=>{
     console.log(`User Connected - ${socket.id}`);
